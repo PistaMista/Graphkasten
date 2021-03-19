10f601b0ffdc4f087bbefc0d00b40bc981a2d32d
Kryštof Albrecht <krystofalbrechtus@gmail.com>
2021-03-19 20:20:19 +0100

    Fixed window geometry.

--- src/graphkasten.cpp
+++ src/graphkasten.cpp
@@ -3,14 +3,14 @@
 Graphkasten::Graphkasten() {
 	set_title("Graphkasten");
 	set_border_width(10);
+	set_default_geometry(300, 200);
 
 	add(m_grid);
 	m_searchbar.set_margin_bottom(10);
+	m_graphview.set_vexpand(true);
 
 	m_grid.attach(m_searchbar, 0, 0, 100, 1);
-	m_grid.attach_next_to(m_graphview, m_searchbar, Gtk::POS_BOTTOM, 100, 20);
-
-	m_grid.set_row_homogeneous(true);
+	m_grid.attach_next_to(m_graphview, m_searchbar, Gtk::POS_BOTTOM, 100, 1);
 
 	m_grid.show_all_children();
 	m_grid.show();
